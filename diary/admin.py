f49ecdabb0ec6772ed59519654a97adab335874e
geemoowy <kovylek.ul@gmail.com>
2025-07-15 12:31:11 +0300

    added Diary model

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Diary
+
+
+@admin.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_filter = ('mood', 'created_at')
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Diary(models.Model):
+    """ """
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='diaries',
+        verbose_name='Пользователь - создатель дневника'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Заголовок записи',
+        help_text='Введите заголовок записи'
+    )
+    content = models.TextField(
+        verbose_name='Содержание записи',
+        help_text='Поделитесь своей записью (текст записи)'
+    )
+    MOOD_CHOICES = [
+        ('😊', 'Счастье'),
+        ('😟', 'Грусть'),
+        ('😡', 'Злость'),
+        ('😫', 'Усталость'),
+        ('🤩', 'Воодушевление'),
+        ('😮', 'Удивление'),
+        ('😍', 'Влюбленность')
+    ]
+    mood = models.CharField(
+        max_length=2,
+        choices=MOOD_CHOICES,
+        blank=True,
+        verbose_name='Настроение',
+        help_text='Выберите ваше настроение'
+    )
+    image = models.ImageField(
+        upload_to='diary_images/',
+        blank=True,
+        verbose_name='Картинка',
+        help_text='Загрузите картинку'
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name='Публичная запись',
+        help_text='Является ли запись публичной?'
+    )
+    read_time = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        verbose_name='Время чтения (мин)'
+    )
+
+    def __str__(self):
+        return self.title
